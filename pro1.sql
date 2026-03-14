77a53c5e10f2e6955164ab6280df1f194937c1ce
Korrapati Pavani <korrapatipavani2428@gmail.com>
2026-03-14 22:04:28 +0530

    Delete pro1.sql

--- pro1.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER notify_high_engagement
-AFTER INSERT ON Engagements1
-FOR EACH ROW
-BEGIN
-  IF (:NEW.likes + :NEW.comments + :NEW.shares) > 300 THEN
-    DBMS_OUTPUT.PUT_LINE('🔥 High engagement detected for Post ID: ' || :NEW.post_id);
-  END IF;
-END;
-/
